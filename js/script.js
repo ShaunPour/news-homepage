f148800f8df2dbd08c8df68942537d074b511e89
Shaun <scpour@gmail.com>
2024-01-29 15:06:03 -0500

    Changed site design to match 1440px design when using the correct size screen & fixed menu bar on mobile to display correct colors and format when hamburger btn is clicked

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu-list');
+const body = document.querySelector('body');
 
 menuIcon.addEventListener('click', () => {
     menu.classList.toggle('menu-hide');
     if (menu.classList.contains('menu-hide')) {
         menuIcon.src = "./assets/images/icon-menu.svg";
+        body.style.background = 'inherit';
     } else {
         menuIcon.src = "./assets/images/icon-menu-close.svg";
+        body.style.background = 'hsl(236, 13%, 42%)';
+        body.style.overflow = 'hidden';
     }
 });
